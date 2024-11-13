6b3fd7b87e4804cd37a31559c63bdd3b65c87a96
Akshay T V <akshay.tv@experionglobal.local>
2024-11-13 14:06:04 +0530

    completed assignment

--- angular-kerala/src/app/app.component.ts
+++ angular-kerala/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyCarousalComponent } from "./ui/my-carousal/my-carousal.component";
+import { MyDescriptionComponent } from "./ui/my-description/my-description.component";
+import { MyCardComponent } from "./ui/my-card/my-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MyCarousalComponent, MyDescriptionComponent, MyCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-kerala';
+  
 }
